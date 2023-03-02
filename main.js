fe4fc0a9859dbae4b3c9c3193cd94ee33fd9a424
Magistry <Magistrigusti@yandex.ru>
2023-03-02 15:28:33 +0200

    0.4

--- main.js
+++ main.js
@@ -1,41 +1,9 @@
 const apiKey = '2c968b7c333349a7b67133121232502';
-
 const header = document.querySelector('.header');
 const form = document.querySelector('#form');
 const input = document.querySelector('#inputCity');
 
-function removeCard() {
-  const prevCard = document.querySelector('.card');
-  if (prevCard) prevCard.remove();
-}
-
-function showError (errorMessage) {
-  const html = `<div class="card">${errorMessage}</div>`;
-  header.insertAdjacentHTML('afterEnd', html);
-}
-
-form.onsubmit = function (event) {
-  event.preventDefault();
-
-  let city = input.value.trim();
-  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
-
-  fetch (url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      if (data.error) {
-        removeCard();
-        showError(data.error.message);
-      } else {
-        removeCard();
-        showCard(data.location.name, 
-                  data.location.country,
-                  data.current.temp_c,
-                  data.current.condition.text);
-
-        function showCard (name, country, temp, condition) {
+function showCard ({name, country, temp, condition}) {
           const html = `
             <div class="card">
               <h2 class="card-city">
@@ -49,7 +17,45 @@ form.onsubmit = function (event) {
 
           header.insertAdjacentHTML('afterEnd', html);
         }
-        
-      }
-    })
+
+function removeCard() {
+  const prevCard = document.querySelector('.card');
+  if (prevCard) prevCard.remove();
+}
+
+function showError (errorMessage) {
+  const html = `<div class="card">${errorMessage}</div>`;
+  header.insertAdjacentHTML('afterEnd', html);
+}
+
+
+
+async function getWeather(city) {
+  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+form.onsubmit = async function (event) {
+  event.preventDefault();
+
+  let city = input.value.trim();
+
+  const data = await getWeather(city);
+
+  if (data.error) {
+    removeCard();
+    showError(data.error.message);
+  } else {
+    removeCard();
+    const weatherData = {
+      name: data.location.name,
+      country: data.location.country,
+      temp: data.current.temp_c,
+      condition: data.current.condition.text
+    } 
+
+    showCard(weatherData);
+  }
 }
